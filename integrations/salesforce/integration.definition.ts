94cabebf8315b28348a5bf0c04cdea5da7da8fe6
ermek-botpress <ermek.barmashev@botpress.com>
2024-09-03 10:27:50 -0400

    fix(salesforce): bump version (#8)

--- integrations/salesforce/integration.definition.ts
+++ integrations/salesforce/integration.definition.ts
@@ -5,7 +5,7 @@ import { actionDefinitions } from "./src/definitions";
 export default new IntegrationDefinition({
   name: integrationName,
   title: "Salesforce",
-  version: "0.4.1",
+  version: "0.4.2",
   readme: "hub.md",
   icon: "icon.svg",
   description:
