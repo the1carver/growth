719c1243c9b9b27c48cfd77fa0d856344a3d27b1
Patrick <87815239+ptrckbp@users.noreply.github.com>
2024-09-17 10:09:21 -0400

    meta changes for integration

--- integrations/hitl-api/integration.definition.ts
+++ integrations/hitl-api/integration.definition.ts
@@ -4,7 +4,10 @@ import { name, integrationName } from "./package.json";
 export default new IntegrationDefinition({
   name: integrationName ?? name,
   version: "0.2.0",
-  readme: "README.md",
+  title:"Human in the Loop API",
+  description: "This integration allows you to connect Botpress to a Human in the Loop / Live Agent API",
+  icon: "icon.svg",
+  readme: "hub.md",
   channels: {
     hitl: {
       messages: { ...messages.defaults },
