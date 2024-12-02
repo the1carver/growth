bab88ece8c7c8c99ad6b611d49fb822a48cae8bf
David Vitor Antonio <david.vitora@gmail.com>
2024-12-02 11:19:27 -0300

    fix multiline

---
+++ integrations/hitl-salesforce/src/events/conversation-message.ts
@@ -0,0 +1,78 @@
+import * as bp from '.botpress'
+import { Conversation } from '@botpress/client'
+import { MessageMessagingTrigger, MessageDataPayload } from "../triggers";
+
+export const executeOnConversationMessage = async ({
+                                               messagingTrigger,
+                                               conversation,
+                                               client,
+                                               logger
+                                           }: {
+    messagingTrigger: MessageMessagingTrigger
+    conversation: Conversation
+    client: bp.Client
+    logger: bp.Logger
+}) => {
+
+    const {
+        sender: {
+            role: senderRole,
+            subject: senderSubject,
+        },
+        senderDisplayName
+    } = messagingTrigger.data.conversationEntry
+
+    if(senderRole == 'EndUser') {
+        return
+    }
+
+    console.log('Got onConversationMessage Trigger messagingTrigger', { senderRole: messagingTrigger.data.conversationEntry.sender.role, messagingTrigger: JSON.stringify(messagingTrigger, null, 2)})
+
+    const { user } = await client.getOrCreateUser({
+        name: senderDisplayName,
+        tags: {
+            id: senderSubject
+        },
+    })
+
+    if(!user.name?.length) {
+        void client.updateUser({
+            ...user,
+            name: senderDisplayName,
+            tags: {
+                id: senderSubject
+            }
+        })
+    }
+
+    let entryPayload: MessageDataPayload;
+
+    try {
+        entryPayload = JSON.parse(messagingTrigger.data.conversationEntry.entryPayload) as MessageDataPayload
+    } catch (e) {
+        console.log('Could not parse entry payload', e)
+        return
+    }
+
+    console.log('Got entry payload: ', { entryPayload })
+
+    if( entryPayload.abstractMessage.messageType !== "StaticContentMessage") {
+        console.log(`Salesforce Messaging HITL does not support messages of type '${entryPayload.abstractMessage.messageType}'`)
+        logger.forBot().error(`Salesforce Messaging HITL does not support messages of type '${entryPayload.abstractMessage.messageType}'` )
+        return
+    }
+
+    if( entryPayload.abstractMessage.staticContent.formatType !== "Text") {
+        console.log(`Salesforce Messaging HITL does not support messages of format type '${entryPayload.abstractMessage.staticContent.formatType}'`)
+        logger.forBot().error(`Salesforce Messaging HITL does not support messages of format type '${entryPayload.abstractMessage.staticContent.formatType}'` )
+        return
+    }
+
+    await client.createMessage({
+        tags: {},
+        type: 'text',
+        userId: user?.id as string,
+        conversationId: conversation.id,
+        payload: { text: `${senderRole == 'Agent' && `${senderDisplayName}: ` || ''}${entryPayload.abstractMessage.staticContent.text}` },
+    })
+}
---
+++ integrations/hitl-salesforce/src/events/participant-changed.ts
@@ -0,0 +1,82 @@
+import * as bp from '.botpress'
+import {
+    ParticipantChangedDataPayload,
+    ParticipantChangedMessagingTrigger
+} from "../triggers";
+import {Conversation} from "@botpress/client";
+import {closeConversation} from "./conversation-close";
+
+export const executeOnParticipantChanged = async ({
+                                               messagingTrigger,
+                                               conversation,
+                                               ctx,
+                                               client,
+                                               logger
+                                           }: {
+    messagingTrigger: ParticipantChangedMessagingTrigger
+    conversation: Conversation
+    ctx: bp.Context
+    client: bp.Client
+    logger: bp.Logger
+}) => {
+
+    let entryPayload: ParticipantChangedDataPayload;
+
+    try {
+        entryPayload = JSON.parse(messagingTrigger.data.conversationEntry.entryPayload) as ParticipantChangedDataPayload
+    } catch (e) {
+        console.log('Could not parse entry payload', e)
+        return
+    }
+
+    for(const entry of entryPayload.entries) {
+        const {
+            displayName,
+            participant: {
+                role,
+                subject
+            }
+        } = entry
+
+        if(role !== 'Agent') {
+            return
+        }
+
+        switch (entry.operation) {
+            case 'remove':
+                console.log('Agent removed, closing conversation')
+
+                await closeConversation({ conversation, ctx, client, logger })
+                return;
+            case 'add':
+                console.log('Agent assigned')
+                const { user } = await client.getOrCreateUser({
+                    name: displayName,
+                    tags: {
+                        id: subject
+                    },
+                })
+
+                if(!user.name?.length) {
+                    await client.updateUser({
+                        ...user,
+                        name: displayName,
+                        tags: {
+                            id: subject
+                        }
+                    })
+                }
+
+                console.log({ agentUser: user })
+
+                await client.createEvent({
+                    type: 'hitlAssigned',
+                    payload: {
+                        conversationId: conversation.id,
+                        userId: user.id,
+                    },
+                })
+        }
+    }
+
+}
